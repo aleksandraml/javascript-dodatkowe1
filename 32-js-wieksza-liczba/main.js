202f769f229fefab3b9c77816a244f43bb98f5e8
aleksandraml <aleksandramentel1525@gmail.com>
2023-01-17 12:52:50 +0100

    aktualizacja

--- 32-js-wieksza-liczba/main.js
+++ 32-js-wieksza-liczba/main.js
@@ -1,7 +1,10 @@
 let num1 = 2;
 let num2 = 5;
 
-function numbers (num1, num2){
-let result = num1  + num2
+function sum(num1, num2){
+let result = num1  + num2;
 return num2
 }
+console.log(sum(num1, num2))
+
+
