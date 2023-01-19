2871204cca973db5d5712009f6671e0fd7e7191c
aleksandraml <aleksandramentel1525@gmail.com>
2023-01-19 12:15:31 +0100

    aktualizacja

--- 36-js-sprawdzanie-liczby2/main.js
+++ 36-js-sprawdzanie-liczby2/main.js
@@ -1,6 +1,7 @@
 function number_ranges(x, y) {
-    if(x === 20 && y === 20 && x + y <= 20)
-      return true
-    }
-    
-    console.log(number_ranges(1, 19))
+  if (x === 20 || y === 20 || x + y <= 20)
+    return true
+}
+
+console.log(number_ranges(1, 19))
+
