b9d4f6de17ddf472e886732d9b9d653f0e2e4c36
aleksandraml <aleksandramentel1525@gmail.com>
2023-01-19 12:22:17 +0100

    poprawiona poprawka

--- 32-js-wieksza-liczba/main.js
+++ 32-js-wieksza-liczba/main.js
@@ -1,10 +1,14 @@
 let num1 = 2;
 let num2 = 5;
 
-function sum(num1, num2){
-let result = num1  + num2;
-return num2
+function sum(num1, num2) {
+    if (num1 > num2) {
+        return num1
+    } else if (num1 < num2) {
+        return num2
+    }
+
 }
-console.log(sum(num1, num2))
+
 
 
