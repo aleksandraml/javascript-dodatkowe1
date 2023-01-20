7ac7c9fa57685a2fd70659c03903bb6a79b57077
aleksandraml <aleksandramentel1525@gmail.com>
2023-01-20 11:35:03 +0100

    poprawka

--- 34-js-dlugosc-stringa/main.js
+++ 34-js-dlugosc-stringa/main.js
@@ -1,6 +1,21 @@
-let text1 = 'Uwielbiam JavaScript';
-let text2 = 'Jestem świetnym programistą';
+const firstText = 'Uwielbiam JavaScript';
+const secondText = 'Jestem świetnym programistą';
 
-function texts(text1, text2){
-    retrun text2
+
+
+function texts(str1, str2) {
+    if (str1.length < str2.length) {
+        return str2
+    } else if (str2.length < str1.length) {
+        return str1
+    }
+
 }
+
+
+texts(firstText, secondText)
+texts('Ulajdilwejhihwi', 'Aleksandra')
+
+console.log(texts(firstText, secondText));
+console.log(texts('Ulajdilwejhihwi', 'Aleksandra'));
+
